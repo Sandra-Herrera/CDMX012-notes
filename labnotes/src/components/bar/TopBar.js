ebc281aa120703aadaeff36a656a33a367dd392a
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-04-25 20:07:49 -0500

    logout & CSS landing

--- labnotes/src/components/bar/TopBar.js
+++ labnotes/src/components/bar/TopBar.js
@@ -1,11 +1,25 @@
 import imgAddNote from "../../img/imgAddNote.png";
+import "./topBar.css";
+import { useNavigate } from "react-router-dom";
 
 export default function TopBar(){
+    const navigate = useNavigate();
+    const logOut = ()=>{
+        navigate('/');
+    }
     return(
         <div>
-            <div>
+            <div class="topBar">
+                <section class="userTemplate">
+                    <img alt="imgProf" class="profilePic" />
+                    <label class="nameTemplate">Karina Hernández</label>
+                </section>
+                <label class="addNoteLabel">Agregar una nota</label>
                 <button class="buttonAddNote">
-                    <img alt="img add note" class="imgAddNote" src={imgAddNote} />
+                    <img alt="imgNote" class="imgAddNote" src={imgAddNote} />
+                </button>
+                <button class="buttonLogOut" onClick={logOut}>
+                    Cerrar sesión
                 </button>
             </div>
         </div>

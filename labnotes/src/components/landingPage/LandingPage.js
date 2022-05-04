86241bdab973d35411171246f944fd8334e7bc5b
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-05-04 15:26:23 -0500

    CSS mejora de colores

--- labnotes/src/components/landingPage/LandingPage.js
+++ labnotes/src/components/landingPage/LandingPage.js
@@ -11,7 +11,7 @@ export default function LandingPage() {
     const navigate = useNavigate();
     useEffect(()=>{
         if(!authentication){
-            navigate('/');
+            //navigate('/');
         }
     },[]);
     

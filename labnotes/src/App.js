306a51654a70defe5767365b6a13381f0424a1c6
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-05-10 02:52:30 -0500

    conexion a github pages

--- labnotes/src/App.js
+++ labnotes/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Login from './components/login/Login';
 import LandingPage from "./components/landingPage/LandingPage";
@@ -7,12 +7,12 @@ import { LoginContextProvider } from "./context/LoginContext";
 function App() {
   return (
     <LoginContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/landing" element={<LandingPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </LoginContextProvider>
   );
 }
